52b3314f0de2be05a570be53317a514829749e6f
Jorge Luna <jorge1980_1@hotmail.com>
2019-01-16 15:26:47 +0100

    refactoring

--- public/appEventos.js
+++ public/appEventos.js
@@ -2,15 +2,14 @@ var app = new Vue({
 	el: '#app',
 
 	data: {
-		eventos: [],
-		eventosD: [],
-		eventosF: [],
-		eventosC: [],
-		eventosP: [],
+		events: [],
+		eventsD: [],
+		eventsF: [],
+		eventsC: [],
+		eventsP: [],
 		eventFilter: [],
-		oculto: "none",
-		auxMap: "",
-		varVolver: "",
+
+		varBack: "",
 		pHome: "",
 		userName: "",
 		email: "",
@@ -19,9 +18,15 @@ var app = new Vue({
 	},
 	created: function () {
 		this.getData();
-		this.createArrays();
-		this.insertNone();
-
+		this.createEventArrays();
+		this.hideSection("calendario");
+		this.hideSection("cultura");
+		this.hideSection("deportes");
+		this.hideSection("festivo");
+		this.hideSection("privado");
+		this.hideSection("eventExtend");
+		this.hideSection("chat");
+		this.hideSection("bodyChat");
 	},
 	computed: {
 
@@ -38,7 +43,7 @@ var app = new Vue({
 	},
 	methods: {
 		getData: function () {
-			fetch("https://api.myjson.com/bins/1chpy8", {
+			fetch("https://api.myjson.com/bins/kyhm4", {
 				method: "GET",
 
 			}).then(function (response) {
@@ -49,17 +54,20 @@ var app = new Vue({
 				}
 
 			}).then(function (json) {
-				app.eventos = json.eventos;
-				app.eventos = app.eventos.sort(function (a, b) {
-					return (b.fecha - a.fecha)
-				})
-				app.createArrays();
-				app.insertNone();
-				app.auxMap;
+				app.events = json.eventos;
+				/*Funcion que oredena los eventos por fecha,accediendo al valor de la key date que es una fecha,la cual divide con "split", la empieza a comparar alrevés gracias a "reverse" y con "join" une la fecha convirtiendola en un número*/
+				app.events.sort(function (a, b) {
+					var aa = a.date.split('/').reverse().join(),
+						bb = b.date.split('/').reverse().join();
+					return aa < bb ? -1 : (aa > bb ? 1 : 0);
+				});
+				app.createEventArrays();
+				app.hideSection();
+
 				app.userName;
 				app.email;
 				app.icon;
-				console.log(app.eventos);
+				console.log(app.events);
 
 			}).catch(function (error) {
 				console.log("Request failed:" + error.message);
@@ -67,121 +75,67 @@ var app = new Vue({
 		},
 
 		/*funcion para crear arrays con los datos y poder acceder a ellos desde el HTML.Están filtrados segun su ID para porner los del mismo tipo juntos*/
-		createArrays: function () {
+		createEventArrays: function () {
 
-			for (var i = 0; i < this.eventos.length; i++) {
+			for (var i = 0; i < this.events.length; i++) {
 
-				if (app.eventos[i].id == "deporte") {
-					this.eventosD.push(app.eventos[i]);
+				if (app.events[i].id == "deporte") {
+					this.eventsD.push(app.events[i]);
 				}
-				if (app.eventos[i].id == "cultura") {
-					this.eventosC.push(app.eventos[i]);
+				if (app.events[i].id == "cultura") {
+					this.eventsC.push(app.events[i]);
 				}
-				if (app.eventos[i].id == "festivo") {
-					this.eventosF.push(app.eventos[i]);
+				if (app.events[i].id == "festivo") {
+					this.eventsF.push(app.events[i]);
 				}
 
-				if (app.eventos[i].id == "privado") {
-					this.eventosP.push(app.eventos[i]);
+				if (app.events[i].id == "privado") {
+					this.eventsP.push(app.events[i]);
 				}
 
 			}
-			console.log(this.eventosD);
-			console.log(this.eventosC);
-			console.log(this.eventosF);
+			console.log(this.eventsD);
+			console.log(this.eventsC);
+			console.log(this.eventsF);
+
 		},
 		/*Función para ocultar de inicio los div que no quiero que se muestren*/
-		insertNone: function () {
+		hideSection: function (section) {
 			var numSelect = 0;
 
-			x = document.getElementById("calendario");
-			x.style.display = this.oculto;
+			divSection = document.getElementById(section);
+			divSection.style.display = "none";
 
-			y = document.getElementById("cultura");
-			y.style.display = this.oculto;
-
-			z = document.getElementById("deportes");
-			z.style.display = this.oculto;
-
-			l = document.getElementById("festivo");
-			l.style.display = this.oculto;
-
-			j = document.getElementById("privado");
-			j.style.display = this.oculto;
-
-			n = document.getElementById("eventoExtend");
-			n.style.display = this.oculto;
-
-			p = document.getElementById("chat");
-			p.style.display = this.oculto;
-
-			q = document.getElementById("bodyChat");
-			q.style.display = this.oculto;
 		},
 		/*función para cambiar de div sugun hagamos click en el boton que elijamos.LO que hace es cambiar el valor del atributo display,uso un parametro para hacerlo mas dinamico y que la funcion me valga para todos los div,en el HTML meteré de parametro el id del div que quiero mostrar*/
-		changeDiv: function (p) {
+		changeDiv: function (parametro1, parametro2) {
 
-			x = document.getElementById("mainDiv");
-			x.style.display = this.oculto;
+			x = document.getElementById(parametro2);
+			x.style.display = "none";
 
-			y = document.getElementById(p);
+			y = document.getElementById(parametro1);
 			y.style.display = "block";
-			this.pHome = p;
-			console.log(p)
-		},
-		changeDiv2: function () {
+			this.pHome = parametro1;
+			this.eventFilter = [];
 
-			x = document.getElementById("mainDiv");
-			x.style.display = "block";
-
-			y = document.getElementById(this.pHome);
-			y.style.display = this.oculto;
 		},
-		/*funcion que mostrará el div con la información completa de cada evento,para ello se rellenará un nuevo array con este evento,para ello la función tiene dos parametros que uno de ellos será el nombre del evento que lo cogerá desde el HTML y de forma dinámica ya que el valor del nombre del evento varía automaticamente.el otro parametro se pondrá dependiendo del div desde el que llamemos al vento ya que ese parametro sera el div que ocultemos para mostrar el nuevo.Por último esta función engresará el mapa por medio de INNERHTML en el div que hemos creado para ello en el HTML,el valor de la variable auxMap sera o que se introduzca de forma dinamica.*/
+		/*funcion que mostrará el div con la información completa de cada evento,para ello se rellenará un nuevo array con este evento,para ello la función tiene dos parametros que uno de ellos será el nombre del evento que lo cogerá desde el HTML y de forma dinámica ya que el valor del nombre del evento varía automaticamente.el otro parametro se pondrá dependiendo del div desde el que llamemos al vento ya que ese parametro sera el div que ocultemos para mostrar el nuevo.*/
 		divExtend: function (p, p2) {
 
-			for (var i = 0; i < this.eventos.length; i++) {
+			for (var i = 0; i < this.events.length; i++) {
 
-				if (app.eventos[i].nombre == p) {
-					this.eventFilter.push(this.eventos[i])
+				if (app.events[i].nombre == p) {
+					this.eventFilter.push(this.events[i])
 					x = document.getElementById(p2);
-					x.style.display = this.oculto;
+					x.style.display = "none";
 
-					y = document.getElementById("eventoExtend");
+					y = document.getElementById("eventExtend");
 					y.style.display = "block";
-
-					this.auxMap = this.eventos[i].map
-					this.varVolver = p2;
-
+					this.varBack = p2;
 				}
 			}
-			console.log(this.auxMap);
-			var q = document.getElementById("map");
-			q.innerHTML = app.auxMap;
-		},
-		volver: function () {
-			this.eventFilter = [];
-			y = document.getElementById("eventoExtend");
-			y.style.display = "none";
-			x = document.getElementById(this.varVolver);
-			x.style.display = "block";
-			var q = document.getElementById("map");
-			q.innerHTML = "";
 
 		},
-		toHome: function () {
-			this.eventFilter = [];
-			x = document.getElementById("eventoExtend");
-			x.style.display = this.oculto;
-
-			y = document.getElementById("mainDiv");
-			y.style.display = "block";
-			var q = document.getElementById("map");
-			q.innerHTML = "";
-
-			console.log(this.pHome)
-		},
-
 		login: function () {
 			// https://firebase.google.com/docs/auth/web/google-signin
 
@@ -269,7 +223,7 @@ var app = new Vue({
 					var element = messages[key];
 					if (firebase.auth().currentUser.displayName == element.name) {
 						globo.setAttribute("class", "globos");
-					}else{
+					} else {
 						globo.setAttribute("class", "globos2");
 					}
 					text.append(element.text);
